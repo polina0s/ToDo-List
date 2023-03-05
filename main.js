92e5e57c36e326954328ea64a59315d3af87d8d1
polina0s <polina.osintseva000@gmail.com>
2023-03-05 19:56:53 +0400

    add events

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-import "./style.css";
+document.addEventListener("DOMContentLoaded", bootstrap);
+
+function bootstrap() {
+  const taskInput = document.querySelector("#taskInput_text");
+  const buttonAdd = document.querySelector("#taskInput_buttonAdd");
+  const tasksList = document.querySelector("#tasksList");
+
+  let tasks = [];
+
+  taskInput.addEventListener("input", (e) => console.log(e.target.value));
+  buttonAdd.addEventListener("click", (e) => {
+    if (taskInput.value !== "") {
+      tasks.push(taskInput.value);
+
+      const task = document.createElement("div"); // <div></div>
+      task.classList.add("task"); // <div class="task"></div>
+      task.innerHTML = `<div class="task_checkbox">
+          <input
+            type="checkbox"
+            class="task_checkbox-custom"
+            id="checkboxCustom"
+          />
+          <label for="checkboxCustom"></label>
+        </div>
+        <div class="task_text">
+          <p class="task_textString">${taskInput.value}</p>
+        </div>
+        <button class="task_deleteButton">
+          <image
+            src="/public/delete.png"
+            alt="delete task"
+            class="task_deleteButton-img"
+          />
+        </button>
+        <button class="task_editButton">
+          <image
+            src="/public/edit.png"
+            alt="edit task"
+            class="task_editButton-img"
+          />
+        </button>`; // <div class="task"> <p>${taskInput.value}</p> </div>
+      tasksList.append(task); //<div class="taskList" id="taskList">  <div class="task"> <p>${taskInput.value}</p> </div> </div>
+
+      taskInput.value = "";
+    }
+  });
+}

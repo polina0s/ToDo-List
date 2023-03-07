0f840c9a34a326581b14b82c78383c02b0d30f4a
polina0s <polina.osintseva000@gmail.com>
2023-03-07 20:38:33 +0400

    add events for delete btn

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function bootstrap() {
   const taskInput = document.querySelector("#taskInput_text");
   const buttonAdd = document.querySelector("#taskInput_buttonAdd");
   const tasksList = document.querySelector("#tasksList");
-
   let tasks = [];
 
   taskInput.addEventListener("input", (e) => console.log(e.target.value));
@@ -12,36 +11,45 @@ function bootstrap() {
     if (taskInput.value !== "") {
       tasks.push(taskInput.value);
 
-      const task = document.createElement("div"); // <div></div>
-      task.classList.add("task"); // <div class="task"></div>
+      const task = document.createElement("div");
+      task.classList.add("task");
       task.innerHTML = `<div class="task_checkbox">
+// TODO: fix
+        <label>
           <input
             type="checkbox"
             class="task_checkbox-custom"
-            id="checkboxCustom"
-          />
-          <label for="checkboxCustom"></label>
+          />    
+          </label>
         </div>
-        <div class="task_text">
-          <p class="task_textString">${taskInput.value}</p>
-        </div>
-        <button class="task_deleteButton">
+        <input
+          type="text"
+          class="task_textString"
+          data-id="task_textString"
+          value='${taskInput.value}'
+        />
+        <button class="task_deleteButton" data-id="task_deleteButton">
           <image
             src="/public/delete.png"
             alt="delete task"
             class="task_deleteButton-img"
           />
-        </button>
-        <button class="task_editButton">
-          <image
-            src="/public/edit.png"
-            alt="edit task"
-            class="task_editButton-img"
-          />
-        </button>`; // <div class="task"> <p>${taskInput.value}</p> </div>
-      tasksList.append(task); //<div class="taskList" id="taskList">  <div class="task"> <p>${taskInput.value}</p> </div> </div>
+        </button>`;
+      tasksList.append(task);
 
       taskInput.value = "";
+
+      const deleteButton = task.querySelector(`[data-id="task_deleteButton"]`);
+
+      deleteButton.addEventListener("click", (e) => {
+        task.remove();
+      });
+
+      // TODO: selector на клик лейбла, находить в нем чекбокс и переключать состояние
+      const checkbox = task.querySelector(`label`);
+      checkbox.addEventListener("click", (e) => {
+        console.log(checkbox.querySelector("input"));
+      });
     }
   });
 }

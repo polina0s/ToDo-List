f371d2d11c92f3568086a34ba419e324e53a1da2
polina0s <polina.osintseva000@gmail.com>
2023-03-09 19:50:09 +0400

    fixed functions

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
 document.addEventListener("DOMContentLoaded", bootstrap);
 
-function bootstrap() {
-  const taskInput = document.querySelector("#taskInput_text");
-  const buttonAdd = document.querySelector("#taskInput_buttonAdd");
-  const tasksList = document.querySelector("#tasksList");
-  let tasks = [];
-  function addDelete() {
-    if (taskInput.value !== "") {
-      tasks.push(taskInput.value);
-
-      const task = document.createElement("div");
-      task.classList.add("task");
-      task.innerHTML = `<div class="task_checkbox">
+function createTask(text) {
+  const task = document.createElement("div");
+  task.classList.add("task");
+  task.innerHTML = `<div class="task_checkbox">
         <label class="task_checkbox-custom">
           <input type="checkbox" />
           <span></span>
@@ -21,7 +13,7 @@ function bootstrap() {
         type="text"
         class="task_textString"
         data-id="task_textString"
-        value="${taskInput.value}"
+        value="${text}"
       />
       <button class="task_deleteButton" data-id="task_deleteButton">
         <image
@@ -30,34 +22,59 @@ function bootstrap() {
           class="task_deleteButton-img"
         />
       </button>`;
+
+  const deleteButton = task.querySelector(`[data-id="task_deleteButton"]`);
+  const taskString = task.querySelector(`[data-id="task_textString"]`);
+
+  deleteButton.addEventListener("click", (e) => {
+    task.remove();
+  });
+
+  const checkbox = task.querySelector(`input`);
+  checkbox.addEventListener("click", (e) => {
+    if (checkbox.checked) {
+      taskString.setAttribute("disabled", "disabled");
+    } else {
+      taskString.removeAttribute("disabled");
+    }
+  });
+
+  return task;
+}
+
+function bootstrap() {
+  const taskInput = document.querySelector("#taskInput_text");
+  const buttonAdd = document.querySelector("#taskInput_buttonAdd");
+  const tasksList = document.querySelector("#tasksList");
+
+  let tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
+
+  for (let i = 0; i < tasks.length; i++) {
+    const task = createTask(tasks[i].text);
+    tasksList.append(task);
+  }
+
+  function addTask() {
+    if (taskInput.value !== "") {
+      tasks.push({
+        text: taskInput.value,
+        checked: false,
+      });
+
+      const task = createTask(taskInput.value);
       tasksList.append(task);
 
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+
       taskInput.value = "";
-
-      const deleteButton = task.querySelector(`[data-id="task_deleteButton"]`);
-      const taskString = task.querySelector(`[data-id="task_textString"]`);
-
-      deleteButton.addEventListener("click", (e) => {
-        task.remove();
-      });
-
-      const checkbox = task.querySelector(`input`);
-      checkbox.addEventListener("click", (e) => {
-        console.log(11111);
-        if (checkbox.checked) {
-          taskString.setAttribute("disabled", "disabled");
-        } else {
-          taskString.removeAttribute("disabled");
-        }
-      });
     }
   }
 
   taskInput.addEventListener("input", (e) => console.log(e.target.value));
-  buttonAdd.addEventListener("click", addDelete);
+  buttonAdd.addEventListener("click", addTask);
   taskInput.addEventListener("keydown", (e) => {
     if (e.code === "Enter") {
-      addDelete();
+      addTask();
     }
   });
 }
